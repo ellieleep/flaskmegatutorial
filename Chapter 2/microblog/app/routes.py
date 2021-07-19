b79bafcdc070ab79ff2dc0524c3b689e2bfbc638
ellieleep <ellie.leepierce@gmail.com>
2021-07-19 16:24:22 -0400

    added files

--- Chapter 2/microblog/app/routes.py
+++ Chapter 2/microblog/app/routes.py
@@ -6,4 +6,4 @@ from flask import render_template
 @app.route('/index')
 def index():
     user = {'username': 'Miguel'}
-    return render_template('index.html', title='Home', user=user)
+    return render_template('index.html', user=user)
